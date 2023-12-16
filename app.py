681c41d931e278be5b272135c72a791d757f5691
HARSHIT SHUKLA <32368309+harshitshukla-github@users.noreply.github.com>
2023-12-16 04:40:33 +0000

    3re Commit Basic Wireframing

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from views import view_blueprint
 
 app = Flask(__name__)
 
-app.register_blueprint(view_blueprint, url_prefix="/")
+app.register_blueprint(view_blueprint, url_prefix="/",  static_url_path='/static')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
