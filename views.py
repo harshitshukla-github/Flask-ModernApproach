7b042a0dafda313231396f0e3cdc07c54e2030a5
HARSHIT SHUKLA <32368309+harshitshukla-github@users.noreply.github.com>
2023-12-19 14:16:34 +0000

    Adding Jinja Template

--- views.py
+++ views.py
@@ -1,11 +1,17 @@
+import json
 from flask import Blueprint, render_template
 
 view_blueprint = Blueprint("view", __name__)
 
+with open("data.json", "r") as file:
+    data = json.load(file)
+
+print(data)
+
 @view_blueprint.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", data=data)
 
 @view_blueprint.route("/home")
 def home1():
-    return render_template("index.html")
+    return render_template("index.html", data=data)
