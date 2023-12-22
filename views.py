11c4b03b853287896b461ecda518efa149dcf542
HARSHIT SHUKLA <32368309+harshitshukla-github@users.noreply.github.com>
2023-12-22 20:49:59 +0000

    Added Redirecting

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 import json
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,redirect, url_for
 
 view_blueprint = Blueprint("view", __name__)
 
@@ -15,3 +15,13 @@ def home():
 @view_blueprint.route("/home")
 def home1():
     return render_template("index.html", data=data)
+
+@view_blueprint.route("/vacancy")
+def vacancy():
+    # Assuming you have a route for the form, let's say 'form'
+    return redirect(url_for("form"))
+
+# Assuming you have a route for the form, let's say 'form'
+@view_blueprint.route("/form")
+def form():
+    return render_template("form.html")
